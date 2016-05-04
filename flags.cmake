0031ef8d01463696e3ca813e26bda3b934f79a91
Matthias Wagner <matzewagner@gmail.com>
2016-05-04 15:38:01 -0700

    changes flags.cmake for cross platform

--- flags.cmake
+++ flags.cmake
@@ -1,2 +1,6 @@
-target_link_libraries("${APP_NAME}" /usr/local/lib/libloris.dylib lo)
+#if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+#    target_link_libraries("${APP_NAME}" /usr/local/lib/libloris.dylib lo)
+#elseif(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+    target_link_libraries("${APP_NAME}" loris lo)
+
 
